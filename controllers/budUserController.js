9a2bd2ec5272e4bd11be27dcd7750f1788dee72f
Hariiikrishnan <therihari36@gmail.com>
2023-06-17 14:57:40 +0530

    Initial 4 Deploy Fail Fix

--- controllers/budUserController.js
+++ controllers/budUserController.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import asyncHandler from "express-async-handler";
 import bodyparser from "body-parser";
 import passport from "passport";
-import passportlocalmongoose from "passport-local-mongoose";
+// import passportlocalmongoose from "passport-local-mongoose";
 
 
 import { fileURLToPath } from 'url';
@@ -15,8 +15,8 @@ import path,{ dirname } from 'path';
 import fs from "fs";
 import axios from "axios";
 // import GoogleStrategy from 'passport-google-oauth20'.Strategy;
-import FacebookStrategy from "passport-facebook"
-import SnapchatStrategy from "passport-snapchat";
+// import FacebookStrategy from "passport-facebook"
+// import SnapchatStrategy from "passport-snapchat";
 
 import {upload,p_uid} from "../utils/uploadImage.js"
 
@@ -90,92 +90,6 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
-// Facebook oAuth
-passport.use(new FacebookStrategy({
-  clientID: process.env.APP_ID,
-  clientSecret: process.env.APP_SECRET,
-  callbackURL: "http://localhost:3001/budgetize/users/auth/facebook/success",
-  profileFields: ['id', 'displayName', 'photos', 'email']
-},
-
-function(accessToken, refreshToken, profile, cb) {
-
-  console.log(profile);
-  console.log(accessToken);
-// To download profile image which was returned by facebook as download link
-const download_image = (url, image_path) =>
-  axios({
-    url,
-    responseType: 'stream',
-  }).then(
-    response =>
-      new Promise((resolve, reject) => {
-        console.log(path.join(__dirname,"../images"));
-        response.data
-          .pipe(fs.createWriteStream(path.join(__dirname,"../images/"+profile.id+".jpg")))
-          .on('finish', () => resolve())
-          .on('error', e => reject(e));
-      }),
-  );
-  
-  (async () => {
-    try{
-      let example_image_1 = await download_image(profile.photos[0].value,profile.id+".jpg");
-  
-      console.log(example_image_1.status); // true
-      console.log(example_image_1.error); // ''
-    }catch(error){
-      console.log("Error ra venna",error)
-    }
-  
-  })();
-  
-
-  BudUser.findOrCreate({ u_id: profile.id ,username:profile.displayName}, function (err, user) {
-    // console.log(user)
-    return cb(err, user);
-  });
-}
-));
-
-// Middlewares and functions of Facebook auth EndPoints. 
-  const fbAuth =  passport.authenticate('facebook')
-
-  const fbAuthMW =  passport.authenticate('facebook', { failureRedirect: '/login' })
-  
-  const fbAuthRedirect = (req,res)=>{
-    res.send("Success")
-  }
-
-  // const magicLogin = new SnapchatStrategy.default();
-
-  try {
-    passport.use(new SnapchatStrategy.default({
-    clientID: process.env.SNAP_APP_ID,
-    clientSecret: process.env.SNAP_APP_SECRET,
-    callbackURL: "http://localhost:3001/budgetize/users/auth/snapchat/callback",
-    profileFields: ['id', 'displayName', 'bitmoji']
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log("inside callback")
-    console.log(profile);
-    console.log(accessToken);
-    BudUser.findOrCreate({ u_id: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-  ));
-} catch(error){
-  console.log("Snap Error", error)
-}
-  console.log("outside callback");
-  
-const snapAuth=passport.authenticate('snapchat',{ scope: ['user.display_name', 'user.bitmoji.avatar'] });
-
-const snapAuthMW=passport.authenticate('snapchat', { failureRedirect: '/login' })
-
-
-
 // Friendly Challenge Handlers
 
 const challengeHandler = asyncHandler(async(req,res)=>{
@@ -208,3 +122,92 @@ const challengers = asyncHandler(async(req,res)=>{
 
 export { authUser, registerUser ,fbAuth,fbAuthMW,fbAuthRedirect,snapAuth,snapAuthMW, challengeHandler};
 
+
+
+// oAuth Implementations
+
+
+
+// Facebook oAuth
+// passport.use(new FacebookStrategy({
+//   clientID: process.env.APP_ID,
+//   clientSecret: process.env.APP_SECRET,
+//   callbackURL: "http://localhost:3001/budgetize/users/auth/facebook/success",
+//   profileFields: ['id', 'displayName', 'photos', 'email']
+// },
+
+// function(accessToken, refreshToken, profile, cb) {
+
+//   console.log(profile);
+//   console.log(accessToken);
+// // To download profile image which was returned by facebook as download link
+// const download_image = (url, image_path) =>
+//   axios({
+//     url,
+//     responseType: 'stream',
+//   }).then(
+//     response =>
+//       new Promise((resolve, reject) => {
+//         console.log(path.join(__dirname,"../images"));
+//         response.data
+//           .pipe(fs.createWriteStream(path.join(__dirname,"../images/"+profile.id+".jpg")))
+//           .on('finish', () => resolve())
+//           .on('error', e => reject(e));
+//       }),
+//   );
+  
+//   (async () => {
+//     try{
+//       let example_image_1 = await download_image(profile.photos[0].value,profile.id+".jpg");
+  
+//       console.log(example_image_1.status); // true
+//       console.log(example_image_1.error); // ''
+//     }catch(error){
+//       console.log("Error ra venna",error)
+//     }
+  
+//   })();
+  
+
+//   BudUser.findOrCreate({ u_id: profile.id ,username:profile.displayName}, function (err, user) {
+//     // console.log(user)
+//     return cb(err, user);
+//   });
+// }
+// ));
+
+// // Middlewares and functions of Facebook auth EndPoints. 
+//   const fbAuth =  passport.authenticate('facebook')
+
+//   const fbAuthMW =  passport.authenticate('facebook', { failureRedirect: '/login' })
+  
+//   const fbAuthRedirect = (req,res)=>{
+//     res.send("Success")
+//   }
+
+//   // const magicLogin = new SnapchatStrategy.default();
+
+//   try {
+//     passport.use(new SnapchatStrategy.default({
+//     clientID: process.env.SNAP_APP_ID,
+//     clientSecret: process.env.SNAP_APP_SECRET,
+//     callbackURL: "http://localhost:3001/budgetize/users/auth/snapchat/callback",
+//     profileFields: ['id', 'displayName', 'bitmoji']
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     console.log("inside callback")
+//     console.log(profile);
+//     console.log(accessToken);
+//     BudUser.findOrCreate({ u_id: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+//   ));
+// } catch(error){
+//   console.log("Snap Error", error)
+// }
+//   console.log("outside callback");
+  
+// const snapAuth=passport.authenticate('snapchat',{ scope: ['user.display_name', 'user.bitmoji.avatar'] });
+
+// const snapAuthMW=passport.authenticate('snapchat', { failureRedirect: '/login' })
