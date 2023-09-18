102d72dade180a4d8de9376357b756c03b7b373b
Hariiikrishnan <therihari36@gmail.com>
2023-09-18 12:14:58 +0530

    changed headers

--- server.js
+++ server.js
@@ -46,15 +46,22 @@ app.use(cors());
 // });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // // res.setHeader(
+  // //   "Access-Control-Allow-Headers",
+  // //   "Origin, X-Requested-With, Content-Type, Accept"
+  // // );
   // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
+  //   "Access-Control-Allow-Methods",
+  //   "GET, POST, PATCH, DELETE, OPTIONS"
   // );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
+
+  res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type,authorization");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+
   next();
 });
 
