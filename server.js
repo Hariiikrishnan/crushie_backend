1ec80899329e01b5ea004256f37b5dc365beaff0
Hariiikrishnan <therihari36@gmail.com>
2023-09-18 11:33:12 +0530

    added acces headers

--- server.js
+++ server.js
@@ -38,11 +38,20 @@ app.use(cors());
 //   credentials:true,
 //   origin:"https://budgetize.netlify.app" || "http://localhost:3000",
 // }));
+
 // app.use(function (request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
+  // response.header("Access-Control-Allow-Origin", "*");
+  // response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // next();
 // });
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 app.use(express.json());
 app.use(session({
