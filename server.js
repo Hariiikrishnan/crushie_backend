bb7d4667066c7bf667c8b2b3297a660d0a423a37
Hariiikrishnan <therihari36@gmail.com>
2023-09-18 12:18:51 +0530

    only localhost

--- server.js
+++ server.js
@@ -32,8 +32,14 @@ app.set("view engine","ejs");
 app.use(bodyparser.urlencoded({extended:true}));
 
 
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
-app.use(cors()); 
+// app.use(cors()); 
 // app.use(cors({
 //   credentials:true,
 //   origin:"https://budgetize.netlify.app" || "http://localhost:3000",
@@ -45,25 +51,25 @@ app.use(cors());
   // next();
 // });
 
-app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // // res.setHeader(
-  // //   "Access-Control-Allow-Headers",
-  // //   "Origin, X-Requested-With, Content-Type, Accept"
-  // // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, PATCH, DELETE, OPTIONS"
-  // );
+// app.use((req, res, next) => {
+//   // res.setHeader("Access-Control-Allow-Origin", "*");
+//   // // res.setHeader(
+//   // //   "Access-Control-Allow-Headers",
+//   // //   "Origin, X-Requested-With, Content-Type, Accept"
+//   // // );
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Methods",
+//   //   "GET, POST, PATCH, DELETE, OPTIONS"
+//   // );
 
-  res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type,authorization");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+// res.setHeader("Access-Control-Allow-Credentials", "true");
+// res.setHeader("Access-Control-Max-Age", "1800");
+// res.setHeader("Access-Control-Allow-Headers", "content-type,authorization");
+// res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 
-  next();
-});
+//   next();
+// });
 
 app.use(express.json());
 app.use(session({
