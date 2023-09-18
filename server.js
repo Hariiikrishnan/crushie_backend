cb57873718550a1e09bad5f3e83c14482dcf2ca9
Hariiikrishnan <therihari36@gmail.com>
2023-09-18 10:50:31 +0530

    fxig cors in server

--- server.js
+++ server.js
@@ -31,10 +31,13 @@ const PORT = process.env.PORT || 3001;
 app.set("view engine","ejs");
 app.use(bodyparser.urlencoded({extended:true}));
 
-// app.use(cors({credentials:true,origin:"https://budgetize.netlify.app" || "http://localhost:3000"}));
 
 
-app.use(cors()); 
+// app.use(cors()); 
+app.use(cors({
+  credentials:true,
+  origin:"https://budgetize.netlify.app" || "http://localhost:3000",
+}));
 // app.use(function (request, response, next) {
 //   response.header("Access-Control-Allow-Origin", "*");
 //   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
